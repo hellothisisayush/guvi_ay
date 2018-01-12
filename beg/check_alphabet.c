a9f564547f6e0897a1c10e1f34f90a4c9669626a
hellothisisayush <ayushagarwal1996@gmail.com>
2018-01-12 21:48:38 +0530

    checkalphabet

---
+++ beg/check_alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("Enter a character:");
+scanf("%c",&ch);
+if(ch>='A' && ch<='Z' !! ch>='a' && ch<='z')
+{
+printf("Alphabet");
+else
+printf("no");
+}
+}

d68e7b9a7d4dffa4d7e67720a236ec9811d24778
hellothisisayush <ayushagarwal1996@gmail.com>
2018-01-15 23:12:39 +0530

    Revert "checkyear"
    This reverts commit 2781f381f453d72b849ffc0110b49b011c2b486a.

--- beg/check_year.c
+++ beg/check_year.c
@@ -3,13 +3,10 @@ void main(){
 int yr;
 printf("enter year");
 scanf("%d",&yr);
-if(yr%400==0)
+if(yr%4==0){
 printf("entered year is a leap year");
-else if(yr%100==0)
+}
+else{
 printf("no, it is not a leap year");
-else if(yr%4==0)
-printf(" it is  a leap year");
-else
-printf("it is not");
-
+}
 }

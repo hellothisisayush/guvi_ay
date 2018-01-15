2781f381f453d72b849ffc0110b49b011c2b486a
hellothisisayush <ayushagarwal1996@gmail.com>
2018-01-15 23:11:49 +0530

    checkyear

--- beg/check_year.c
+++ beg/check_year.c
@@ -3,10 +3,13 @@ void main(){
 int yr;
 printf("enter year");
 scanf("%d",&yr);
-if(yr%4==0){
+if(yr%400==0)
 printf("entered year is a leap year");
-}
-else{
+else if(yr%100==0)
 printf("no, it is not a leap year");
-}
+else if(yr%4==0)
+printf(" it is  a leap year");
+else
+printf("it is not");
+
 }

82f8fde68ce9012a228a0257cc89e46ffcb18683
hellothisisayush <ayushagarwal1996@gmail.com>
2018-01-29 22:03:54 +0530

    primerange

---
+++ player/prime_range.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,a,b,j;
+    printf("enter starting range:");
+    scanf("%d",&a);
+    printf("enter last range");
+    scanf("%d",&b);
+    for(i=a+1;i<b;i++){
+            int prime=1;
+    for(j=2;j<i/2;j++){
+        if(i%j==0){
+            prime=0;
+        }
+    }
+    if(prime==1){
+        printf("%d",i);
+    }
+    }
+
+
+    return 0;
+}

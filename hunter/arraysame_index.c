06d1cd4e43f9d3da1f693e4562d8d4a972e6237d
hellothisisayush <ayushagarwal1996@gmail.com>
2018-03-15 22:23:11 +0530

    arraysameindex

---
+++ hunter/arraysame_index.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+int a[20],b[20],i,j=0,n;
+printf("enter number of elements");
+scanf("%d",&n);
+for(i=0;i<n;i++){
+    scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++){
+    if(a[i]==i){
+        b[j]=i;
+        j++;
+    }
+}
+if(j==0){
+    printf("-1");
+}
+else{
+for(i=0;i<j;i++)
+    printf("%d\t",b[i]);
+}
+
+return 0;
+}
